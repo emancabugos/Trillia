e97656e46a6ffc0c65dcaa52716622366f9f1ba8
April <April@April-PC>
2019-07-03 08:19:10 +0800

    07/03/2019 - Checkout Default Flow

--- Scripts/CONSUMER/Checkout Scenario/Checkout_DefaultFlow/Script1561618375092.groovy
+++ Scripts/CONSUMER/Checkout Scenario/Checkout_DefaultFlow/Script1561618375092.groovy
@@ -2,6 +2,7 @@ import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
 import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import org.stringtemplate.v4.compiler.STParser.namedArg_return as namedArg_return
 import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
 import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
@@ -38,24 +39,23 @@ WebUI.waitForElementVisible(findTestObject('CONSUMER/Item Details Page/button_Ad
 
 WebUI.waitForElementVisible(findTestObject('CONSUMER/Item Details Page/textlabel_MOQValue'), 0)
 
-not_run: WebUI.comment('Computation')
+WebUI.comment('Computation')
 
-not_run: def price = 100 /* = WebUI.getText(findTestObject('CONSUMER/Item Details Page/textlabel_price'))*/ 
+def price = WebUI.getText(findTestObject('CONSUMER/Item Details Page/textlabel_price'))
 
-//KeywordLogger log = new KeywordLogger()
-//log.logInfo(price)
+KeywordLogger log = new KeywordLogger()
+
+log.logInfo(price)
+
 WebUI.setText(findTestObject('CONSUMER/Item Details Page/domainUpDown_Quantity'), '10')
 
-not_run: def quantity = 10 /*WebUI.getText(findTestObject('CONSUMER/Item Details Page/domainUpDown_Quantity'))*/ 
+def quantity = 10 /*WebUI.getText(findTestObject('CONSUMER/Item Details Page/domainUpDown_Quantity'))*/ 
 
 //log.logInfo(quantity)
-not_run: def total
+def total = price * quantity
 
-not_run: assert (price * quantity) == total
-
-not_run: KeywordLogger log = new KeywordLogger()
-
-not_run: log.logInfo(total)
+//assert total
+log.logInfo(total)
 
 WebUI.click(findTestObject('CONSUMER/Evaluation Widget/button_up'))
 
@@ -90,9 +90,80 @@ WebUI.waitForElementVisible(findTestObject('CONSUMER/Evaluation Widget/a_Delete/
 
 WebUI.click(findTestObject('CONSUMER/Evaluation Widget/button_EvaluateItems'))
 
+WebUI.comment('Evaluation Table')
+
 WebUI.waitForElementVisible(findTestObject('CONSUMER/Evaluation Table/button_OrderNow'), 0)
 
 WebUI.verifyElementText(findTestObject('CONSUMER/Evaluation Table/textlabel_ItemName'), 'Item Test Checkout')
 
 WebUI.click(findTestObject('CONSUMER/Evaluation Table/button_OrderNow'))
+
+WebUI.comment('Delivery Checkout Page')
+
+WebUI.waitForElementVisible(findTestObject('CONSUMER/Delivery Checkout Page/textlabel_FirstName'), 0)
+
+name = WebUI.getText(findTestObject('CONSUMER/Delivery Checkout Page/default_Name'))
+
+address = WebUI.getText(findTestObject('CONSUMER/Delivery Checkout Page/default_Address'))
+
+country = WebUI.getText(findTestObject('CONSUMER/Delivery Checkout Page/default_Country'))
+
+state = WebUI.getText(findTestObject('CONSUMER/Delivery Checkout Page/default_State'))
+
+city = WebUI.getText(findTestObject('CONSUMER/Delivery Checkout Page/default_City'))
+
+postalcode = WebUI.getText(findTestObject('CONSUMER/Delivery Checkout Page/default_PostalCode'))
+
+WebUI.click(findTestObject('CONSUMER/Delivery Checkout Page/default_Delivery'))
+
+WebUI.scrollToElement(findTestObject('CONSUMER/Delivery Checkout Page/button_Next'), 0)
+
+WebUI.click(findTestObject('CONSUMER/Delivery Checkout Page/button_Next'))
+
+WebUI.comment('Checkout Review Page')
+
+WebUI.waitForElementVisible(findTestObject('CONSUMER/Checkout Review Page/button_ConfirmOrder'), 0)
+
+WebUI.verifyElementText(findTestObject('CONSUMER/Checkout Review Page/textlabel_ConsumerName'), name)
+
+WebUI.verifyElementText(findTestObject('CONSUMER/Checkout Review Page/textlabel_Address'), address)
+
+WebUI.verifyElementText(findTestObject('CONSUMER/Checkout Review Page/textlabel_Country'), country)
+
+WebUI.verifyElementText(findTestObject('CONSUMER/Checkout Review Page/textlabel_City'), city)
+
+WebUI.verifyElementText(findTestObject('CONSUMER/Checkout Review Page/textlabel_State'), state)
+
+WebUI.verifyElementText(findTestObject('CONSUMER/Checkout Review Page/textlabel_PostalCode'), postalcode)
+
+WebUI.click(findTestObject('CONSUMER/Checkout Review Page/button_radio1'))
+
+WebUI.comment('Verification of computation')
+
+WebUI.delay(1)
+
+WebUI.click(findTestObject('CONSUMER/Checkout Review Page/button_ConfirmOrder'))
+
+WebUI.waitForElementVisible(findTestObject('CONSUMER/Transaction Complete Page/textlabel_InvoiceIDValue'), 0)
+
+invoice = WebUI.getText(findTestObject('CONSUMER/Transaction Complete Page/textlabel_InvoiceIDValue'))
+
+WebUI.delay(1)
+
+WebUI.click(findTestObject('CONSUMER/Transaction Complete Page/button_PurchaseHistory'))
+
+WebUI.comment('Purchase History List')
+
+WebUI.waitForElementVisible(findTestObject('CONSUMER/Purchase History List/textfield_Search'), 0)
+
+WebUI.sendKeys(findTestObject('CONSUMER/Purchase History List/textfield_Search'), invoice.toLowerCase())
+
+WebUI.delay(1)
+
+WebUI.waitForElementAttributeValue(findTestObject('CONSUMER/Purchase History List/tablecontent_Invoice'), 'text', invoice, 
+    5)
+
+WebUI.verifyElementText(findTestObject('CONSUMER/Purchase History List/tablecontent_Invoice'), invoice.toLowerCase())
+
+WebUI.acceptAlert()
 
