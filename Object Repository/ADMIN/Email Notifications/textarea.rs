71cab13061652013c454afa16cc26fef47254eb1
Arcadier <Arcadier@Arcadier-PC>
2019-10-08 14:46:33 +0800

    Received Order

---
+++ Object Repository/ADMIN/Email Notifications/textarea.rs
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>textarea</name>
+   <tag></tag>
+   <elementGuidId>958576ba-5f9f-4469-9789-9d867b5c4027</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>textarea</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>content</value>
+   </webElementProperties>
+</WebElementEntity>
