c98c0caab60efb845d88074ef28d0507e4eae51b
ARCADIER01 <ARCADIER01@ARCADIER-01>
2019-11-06 16:19:39 +0800

    google analytics

---
+++ Object Repository/ADMIN/Google Analytics/textlabel_tracking ID Steps.rs
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>textlabel_tracking ID Steps</name>
+   <tag></tag>
+   <elementGuidId>58f114c0-afa1-4f8d-a002-d91d074857f3</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>panel-box-content</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                        
+                            Go to https://www.google.com/analytics/web/.
+                            Login or sign up with a Google account.
+                            Click Sign Up.
+                            What would you like to track? : Select Website.
+                            Account Name : Add your marketplace domain, excluding https or www. (e.g. mymarketplace.arctick.com)
+                            Website Name : Add your full marketplace domain name, excluding https. (e.g. www.mymarketplace.arctick.com)
+                            Website URL : Select https:// from the dropdown and add your full domain name. (e.g. www.mymarketplace.arctick.com)
+                            Industry Category: Choose the industry that is relevant.
+                            Select your TIme Zone.
+                            Data Sharing Settings : Check the boxes that are relevant.
+                            After filling in your information, click on Get Tracking ID.
+                            Accept the terms.
+                            Take note of your Tracking ID that will be generated. It will look something like this : &quot;UA-12345-12&quot;.
+                        
+                    </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;analytics-content&quot;)/div[@class=&quot;gutter-wrapper&quot;]/div[@class=&quot;analytics-panels&quot;]/div[@class=&quot;panel-box goo-panel-box&quot;]/div[@class=&quot;panel-box-content&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Test Suites/Admin/Google Analytics/New Test Suite.ts
@@ -0,0 +1,134 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2019-11-06T16:17:20</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>37565d70-906c-4201-9e3b-ad7bbae6dc18</testSuiteGuid>
+   <testCaseLink>
+      <guid>cf4df2b4-ba0f-4b04-8a56-81a445a3151a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Utilities/ADMIN/Admin Login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dbe5687f-e27e-4987-a9e9-edf9bd1dfa51</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ADMIN/Google Analytics/Navigation Google Analytics</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9bcda8fa-b3f9-485a-a59b-077e6cc88fa3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ADMIN/Google Analytics/01_iconGoogle analytics</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>acd08bce-8b97-4f17-adea-1d489164d204</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ADMIN/Google Analytics/02_textlabel Google Analytics</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>43955e2d-86c3-4012-a90c-0d3d0380e19d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ADMIN/Google Analytics/03_textlabel Google analytics Desc</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e2120595-c244-4e63-8174-fd666dd626d3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ADMIN/Google Analytics/04_icon Enabling Google Analytics</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6fcd04dc-c1fb-4141-b656-ed750bfa7570</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ADMIN/Google Analytics/05_textlabel Enabing Google Analytics</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f0fce25e-55da-411e-895b-d1fa67baec07</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ADMIN/Google Analytics/06_textlabel Be up to date with</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a3ab124a-2d64-4583-bd98-711fecce02f5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ADMIN/Google Analytics/07_button Go to Google Developers</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>58de6fad-4899-4a88-8b2a-c84c6ad6ec9c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ADMIN/Google Analytics/08_textlabel Google Analytics tracking ID</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d7b13c18-debd-4199-8f66-cec193c15cf7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ADMIN/Google Analytics/09_textfield Google Analytics tracking ID</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>54a4aa35-69f6-4300-b196-bd50995f6e3f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ADMIN/Google Analytics/10_textlabel GOOGLE VIEW ID</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5b14c72f-a895-4dcc-b417-7b31fa7dedac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ADMIN/Google Analytics/11_textfield GOOGLE VIEW ID</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b1f77f4a-85df-4163-91f0-cbbcf7ac58a0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ADMIN/Google Analytics/12_button Link ID</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c892c1da-1623-4cd3-b5ae-f27b65da7ee5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ADMIN/Google Analytics/13_linktext How do I get my own Tracking ID</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f25faa0d-2421-434f-bd81-f445a12917b4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ADMIN/Google Analytics/14_linktext How do I get my own View ID</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>164f659a-9bf7-4f17-bd32-46f60f6076b7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ADMIN/Google Analytics/15_toggle Tracking ID</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>36dc213f-b36d-4725-b723-ce8e47716190</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ADMIN/Google Analytics/16_toggle View ID</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>39f28976-5922-4e82-83da-4e950e33c13f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ADMIN/Google Analytics/17_textlabel Tracking ID Steps</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ed0fcbd1-e9de-4701-95bc-064df0b30010</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ADMIN/Google Analytics/18_textlabel View ID Steps</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
