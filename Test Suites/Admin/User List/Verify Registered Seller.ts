077110112bad0ae188801b5ed67228b1ec24149a
Arcadier <Arcadier@Arcadier-PC>
2019-12-27 15:46:31 +0800

    Verify Registered User

---
+++ Test Suites/Admin/User List/Verify Registered Seller.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Verify Registered Seller</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>c479210a-b207-467c-93a3-e6f5109f55c9</testSuiteGuid>
+   <testCaseLink>
+      <guid>43bc3cf6-6662-4cdb-9d96-6940f9bf2e1c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Utilities/ADMIN/Admin Login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2a04ac5d-abc3-49c2-a6bf-ef07844577d2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ADMIN/User Mgmt/Users/Verify Registered User(Seller)</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>22f96eaa-bd87-46be-b0b6-09d04b8aebec</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/ADMIN/Seller List</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>22f96eaa-bd87-46be-b0b6-09d04b8aebec</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Display Name</value>
+         <variableId>3183c25e-1a47-4e1b-bcb9-316f028d7c4d</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4ace9730-3315-461d-a97f-08a33448c89a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Utilities/ADMIN/Admin Logout</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
