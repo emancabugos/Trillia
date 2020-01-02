7b753362d336567f05a04badf88c8c748e2edfb3
ARCADIER01 <ARCADIER01@ARCADIER-01>
2020-01-02 10:17:39 +0800

    verify stocks

---
+++ Scripts/CONSUMER/Transaction Complete Page/Complex/Check Item Stocks/Script1577930584861.groovy
@@ -0,0 +1,32 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.click(findTestObject('CONSUMER/Transaction Complete Page/linktext_ReturnHome'))
+
+WebUI.setText(findTestObject('CONSUMER/Homepage/textfield_Search'), 'item02')
+
+WebUI.sendKeys(findTestObject('CONSUMER/Homepage/textfield_Search'), Keys.chord(Keys.ENTER))
+
+WebUI.click(findTestObject('CONSUMER/Search Result Page/Item02/Item Name_Item02'))
+
+stockvaluesafter = WebUI.getText(findTestObject('CONSUMER/Item Details Page/textlabel_TotalStocksValue'))
+
+WebUI.comment('Add first an expected value')
+
+WebUI.verifyMatch(stockvaluesafter, '', false)
+
